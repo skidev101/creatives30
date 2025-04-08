d4f638cca8229211ac2951168279dba1d14050c0
skidev101 <skidev101@gmail.com>
2025-04-08 22:39:21 +0100

    modified authController

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,8 +1,7 @@
 const User = require('../models/User');
-const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 
-const handleNewUser = async (req, res) => {
+const handleLogin = async (req, res) => {
   const {email, pwd} = req.body;
   if (!email || !pwd) return res.status(400).send("Empty request");
   const { uid } = req.user;
@@ -12,31 +11,18 @@ const handleNewUser = async (req, res) => {
   
   
   try {
-<<<<<<< HEAD
     const user = await User.findOne({uid});
     if (!user) return res.status(402).send('user not found');
-    res.send(user);
-=======
-    let foundUser = await User.findOne({uid});
-    if (foundUser) return res.status(409).send('user already exists');
     
-    const hashedPwd = await bcrypt.hash(pwd, 10);
-    const username = `@user_${uid.slice(0,5)}`;
-    const user = await User.create({
-      uid,
-      email,
-      username,
-      password: hashedPwd
-    });
     if (user) {
       console.log(user);
-      res.status(201).send(`user ${username} created successfully with role${user.roles}`);
+      res.status(200).send(`user ${username} with role${user.roles} is now logged in!`);
     }
->>>>>>> ec45ef28edc2679e2c6263055608a703cef2ea6c
+
   } catch(err) {
     console.log(err);
     res.status(500).send('Internal server error');
   }
 }
 
-module.exports = { handleNewUser }
+module.exports = { handleLogin }
