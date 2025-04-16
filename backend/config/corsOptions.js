d1069e0345b99c4f213b84a21a602f0acd06d059
skidev101 <skidev101@gmail.com>
2025-04-16 12:26:38 +0100

    added

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,6 +1,7 @@
 const whiteList = [
 	'http://127.0.0.1:5173',
-	'http://localhost:5173'
+	'http://localhost:5173',
+	'https://creatives30.vercel.app'
 ];
 
 const corsOptions = {
@@ -11,7 +12,7 @@ const corsOptions = {
 			callback(new Error('not allowed by CORS'))
 		}
 	},
-	methods: ['GET', 'POST','PUT'],
+	methods: ['GET', 'POST', 'PUT'],
 	allowedHeaders: ['Content-Type', 'Authorization'],
 	credentials: true,
 	optionsSuccessStatus: 200
