883a9093c420bce13aacda9612af474afd003dcd
skidev101 <skidev101@gmail.com>
2025-05-01 11:57:36 +0100

    added

--- backend/routes/resetPassword.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { forgotPassword } = require('../controllers/passwordResetController');
-
-router.post('/', forgotPassword);
-
-module.exports = router
